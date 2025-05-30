6cabc73f0b6b01a9adf8f733640ef2b212e66aad
rjlacanlaled <rjlacanlaled@gmail.com>
2025-05-31 06:51:54 +0800

    feat: implement upload request endpoint for wallet generation

--- src/PaylKoyn.Node/Endpoints/UploadRequest.cs
+++ src/PaylKoyn.Node/Endpoints/UploadRequest.cs
@@ -1,19 +1,20 @@
 using FastEndpoints;
+using PaylKoyn.Data.Models;
 using PaylKoyn.Node.Services;
 
-namespace PaylKoyn.Node.Endpoints.Wallet;
+namespace PaylKoyn.Node.Endpoints;
 
-public class Test(WalletService walletService) : EndpointWithoutRequest
+public class UploadRequest(WalletService walletService) : EndpointWithoutRequest
 {
     public override void Configure()
     {
-        Post("/wallet/generate");
+        Post("/upload/request");
         AllowAnonymous();
     }
 
     public override async Task HandleAsync(CancellationToken ct)
     {
-        PaylKoyn.Data.Models.Wallet wallet = await walletService.GenerateWalletAsync();
+        Wallet wallet = await walletService.GenerateWalletAsync();
         string address = wallet.Address;
 
         await SendOkAsync(address, ct);
