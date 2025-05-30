1b41a0cb9bfc934bc3c0fa928dd3da1f6fce3fcb
CML90 <caitlincebu@hotmail.com>
2025-05-30 23:22:18 +0800

    feat: bootstrapped payl koyn web

---
+++ src/PaylKoyn.Web/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+using MudBlazor;
+
+namespace PaylKoyn.Web.Components.Layout;
+
+public partial class MainLayout
+{
+    public MudTheme PaylKoynTheme => new()
+    {
+        PaletteDark = new()
+        {
+            Background = "#000000"
+        },
+        PaletteLight = new()
+        {
+            Background = "#FFFFFF"
+        }
+    };
+}
