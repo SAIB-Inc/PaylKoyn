34bcd3a15fc4d3344da50678da20cd1662bc8759
CML90 <caitlincebu@hotmail.com>
2025-05-31 05:02:11 +0800

    feat: implemented landing pace

--- src/PaylKoyn.Web/Components/Layout/MainLayout.razor.cs
+++ src/PaylKoyn.Web/Components/Layout/MainLayout.razor.cs
@@ -1,18 +1,38 @@
 using MudBlazor;
 
 namespace PaylKoyn.Web.Components.Layout;
-
 public partial class MainLayout
 {
     public MudTheme PaylKoynTheme => new()
     {
-        PaletteDark = new()
-        {
-            Background = "#000000"
-        },
         PaletteLight = new()
         {
-            Background = "#FFFFFF"
+            Background = "#5438DC",
+            Primary = "#FFFFFF"
+        },
+        Typography = new()
+        {
+            H1 = new H1Typography()
+            {
+                FontFamily = [ "Oxanium" ],
+                FontSize = "64px",
+                FontWeight = "700",
+                LineHeight = "72px",
+                LetterSpacing = "0%"
+            },
+            H2 = new H2Typography()
+            {
+                FontFamily = [ "Oxanium" ],
+                FontSize = "56px",
+                FontWeight = "800",
+                LineHeight = "72px",
+                LetterSpacing = "0%"
+            },
+            Default = new DefaultTypography()
+            {
+                FontFamily = ["Inter"]
+            }
         }
+
     };
 }
