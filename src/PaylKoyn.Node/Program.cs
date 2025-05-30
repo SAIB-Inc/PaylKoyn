137f7c6e592481bcbfa198732ea0022b34ee4658
rjlacanlaled <rjlacanlaled@gmail.com>
2025-05-31 03:18:41 +0800

    feat: implement file upload functionality in Program.cs

--- src/PaylKoyn.Node/Program.cs
+++ src/PaylKoyn.Node/Program.cs
@@ -1,5 +1,7 @@
 using Chrysalis.Tx.Models;
 using Chrysalis.Tx.Providers;
+using Chrysalis.Wallet.Models.Keys;
+using Chrysalis.Wallet.Words;
 using Microsoft.EntityFrameworkCore;
 using PaylKoyn.Data.Models;
 using PaylKoyn.Data.Services;
@@ -13,11 +15,29 @@ builder.Services.AddDbContextFactory<WalletDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSingleton<WalletService>();
+builder.Services.AddSingleton<FileService>();
 
 WebApplication app = builder.Build();
 
 IDbContextFactory<WalletDbContext> dbContextFactory = app.Services.GetRequiredService<IDbContextFactory<WalletDbContext>>();
 using WalletDbContext dbContext = dbContextFactory.CreateDbContext();
 dbContext.Database.Migrate();
+
+var seed = Mnemonic.Generate(English.Words, 24);
+Console.WriteLine(string.Join(" ", seed.Words));
+var fileService = app.Services.GetRequiredService<FileService>();
+var wallet = await fileService.RequestUploadAsync();
+var fileContent = new byte[] { 0x01, 0x02, 0x03 };
+var contentType = "application/octet-stream";
+var fileName = "testfile.bin";
+
+try
+{
+    await fileService.UploadAsync(wallet.Address, fileContent, contentType, fileName);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error during file upload: {ex.Message}");
+}
 
 app.Run();
