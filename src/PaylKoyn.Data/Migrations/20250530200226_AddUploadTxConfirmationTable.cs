7a9902d7444520382da9864e9ce5cb926168c4b1
rjlacanlaled <rjlacanlaled@gmail.com>
2025-05-31 04:04:14 +0800

    feat: add UploadTxConfirmation table and update model snapshot

---
+++ src/PaylKoyn.Data/Migrations/20250530200226_AddUploadTxConfirmationTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaylKoyn.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUploadTxConfirmationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UploadTxConfirmations",
+                columns: table => new
+                {
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    TxHash = table.Column<string>(type: "TEXT", nullable: false),
+                    IsConfirmed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TxConfirmationsJson = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UploadTxConfirmations", x => x.Address);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UploadTxConfirmations");
+        }
+    }
+}
