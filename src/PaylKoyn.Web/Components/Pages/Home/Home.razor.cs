528ec610a15b37a3b75891e8b0e5c04f1ca2a8df
KeeeeEf <kfrgallego@gmail.com>
2025-06-02 23:52:36 +0800

    feat(web): add meta tags and open graph

--- src/PaylKoyn.Web/Components/Pages/Home/Home.razor.cs
+++ src/PaylKoyn.Web/Components/Pages/Home/Home.razor.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Components;
+
 namespace PaylKoyn.Web.Components.Pages.Home;
 
 public partial class Home
 {
-    
+    [Inject]
+    public required NavigationManager Navigation { get; set; }
+
+    #region Open Graph Tags
+
+    protected string OgUrl => $"{Navigation.BaseUri}{Navigation.ToBaseRelativePath(Navigation.Uri)}";
+    protected string OgImageUrl => $"{Navigation.BaseUri}images/paylkoyn_og.webp";
+
+    #endregion
 }
