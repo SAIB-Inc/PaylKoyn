61bd2afe8449e58a2acbc2c5a0d99d0a9dc0782b
CML90 <caitlincebu@hotmail.com>
2025-06-02 23:31:33 +0800

    feat: update upload component layout for responsiveness

--- src/PaylKoyn.Web/Components/Pages/Upload/Upload.razor.cs
+++ src/PaylKoyn.Web/Components/Pages/Upload/Upload.razor.cs
@@ -10,8 +10,8 @@ public partial class Upload
     [Inject]
     public required IconService IconService { get; set; }
     
-    [Inject]
-    public required UploadService UploadService { get; set; }
+    // [Inject]
+    // public required UploadService UploadService { get; set; }
 
     protected readonly List<UploadFileState> FileList = [];
     protected string? UploadAddress;
@@ -19,8 +19,8 @@ public partial class Upload
     protected override async Task OnInitializedAsync()
     {
         // Request upload address on page load
-        var response = await UploadService.RequestUploadAsync();
-        UploadAddress = response?.Id;
+        // var response = await UploadService.RequestUploadAsync();
+        //UploadAddress = response?.Id;
         StateHasChanged();
     }
 
@@ -29,11 +29,11 @@ public partial class Upload
         foreach (var file in e.GetMultipleFiles())
         {
             // Estimate fee for this file
-            var feeResponse = await UploadService.EstimateFeeAsync(file.Size);
+            //var feeResponse = await UploadService.EstimateFeeAsync(file.Size);
             var uploadState = new UploadFileState
             {
                 File = file,
-                EstimatedFee = feeResponse?.EstimatedFee ?? 0,
+                //EstimatedFee = feeResponse?.EstimatedFee ?? 0,
                 Status = UploadStatus.Ready
             };
             
@@ -49,10 +49,10 @@ public partial class Upload
         uploadState.Status = UploadStatus.Uploading;
         StateHasChanged();
 
-        var (isSuccess, message) = await UploadService.UploadFileAsync(UploadAddress, uploadState.File);
+        //var (isSuccess, message) = await UploadService.UploadFileAsync(UploadAddress, uploadState.File);
         
-        uploadState.Status = isSuccess ? UploadStatus.Completed : UploadStatus.Error;
-        uploadState.Message = message;
+        // uploadState.Status = isSuccess ? UploadStatus.Completed : UploadStatus.Error;
+        // uploadState.Message = message;
         StateHasChanged();
     }
 
