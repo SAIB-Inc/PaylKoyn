9cfb17f3560ce9f9893de2cd2dc620e79479c8e1
rjlacanlaled <rjlacanlaled@gmail.com>
2025-06-04 18:17:26 +0800

    feat: add NftNumber index and update model snapshot for MintRequest

---
+++ src/PaylKoyn.ImageGen/Migrations/20250604101632_NftNumberIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaylKoyn.ImageGen.Migrations
+{
+    /// <inheritdoc />
+    public partial class NftNumberIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MintRequests_NftNumber",
+                table: "MintRequests",
+                column: "NftNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MintRequests_NftNumber",
+                table: "MintRequests");
+        }
+    }
+}
