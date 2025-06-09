575656ca9a76eb27c5bdc3c90e5ea7a32ddafa84
ricomiles <ricoquiblat@gmail.com>
2025-06-09 23:34:28 +0800

    feat: implement airdrop batching

--- src/PaylKoyn.Data/Models/Template/MultiAssetTransferParams.cs
+++ src/PaylKoyn.Data/Models/Template/MultiAssetTransferParams.cs
@@ -2,11 +2,12 @@ using Chrysalis.Tx.Models;
 
 namespace PaylKoyn.Data.Models.Template;
 
-public record MultiAssetTransferParams(string From, string To, Dictionary<string, Dictionary<string, ulong>> Assets) : ITransactionParameters
+public record Recipient(string Address, Dictionary<string, Dictionary<string, ulong>> Assets);
+
+public record MultiAssetTransferParams(string From, List<Recipient> Recipients) : ITransactionParameters
 {
-    public Dictionary<string, (string address, bool isChange)> Parties { get; set; } = new()
-    {
-        { "change", (From, true) },
-        { "to", (To, false) },
-    };
+    public Dictionary<string, (string address, bool isChange)> Parties { get; set; } = 
+        Recipients.ToDictionary(r => r.Address, r => (r.Address, false))
+        .Append(new KeyValuePair<string, (string, bool)>("change", (From, true)))
+        .ToDictionary();
 }
