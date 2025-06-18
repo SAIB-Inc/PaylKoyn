f17f6ad316683cf7b86ee2011f3a4bd71148eaa7
WendellMorTamayo <tamayowendell05@gmail.com>
2025-06-18 23:59:31 +0800

    feat(data): add composite index on SpentSlot and Slot in OutputsBySlot table

---
+++ src/PaylKoyn.Data/Migrations/20250618152650_AddIndex_OutputsBySlot_SpentSlot_Slot.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaylKoyn.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex_OutputsBySlot_SpentSlot_Slot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OutputsBySlot_SpentSlot_Slot",
+                schema: "public",
+                table: "OutputsBySlot",
+                columns: new[] { "SpentSlot", "Slot" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutputsBySlot_SpentSlot_Slot",
+                schema: "public",
+                table: "OutputsBySlot");
+        }
+    }
+}
